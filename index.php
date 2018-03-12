ebed28e71c658bebb5ad25ff71f599a7afbb4592
Irais Gopar <igopar-osorio@csumb.edu>
2018-03-12 06:14:51 +0000

    Almost finished

--- index.php
+++ index.php
@@ -2,18 +2,13 @@
 $countryNum = $_GET['select'];
 $CountryPics;
 $Cfolder;
+$backgroundImage = 'w3.jpg';
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>Forms</title>
         <link href ="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel = "stylesheet">
-        <style>
-            @import url("css/style.css");
-            body{
-                background-image:url(<?=$backgroundImage?>);
-            }
-        </style>
     </head>
     <body>
         <?php
@@ -37,14 +32,14 @@ $Cfolder;
          
          <form>
     <fieldset>
-        <legend>Music on/off:</legend>
+        <!--<legend>Music on/off:</legend>-->
         <input id="Music" type="radio" name="music" value="On">
         <label for="Music">Music On</label><br>
         <input id="Silent" type="radio" name="music" value="Off">
         <label for="Silent">Music Off</label><br>
         
     </fieldset>
-    <label for="favC">Choose your favorite country?</label>
+    <label for="favC" class = "country">Choose your favorite country?</label>
     <select id="favC" name="select">
         <option value="1">Australia</option>
         <option value="2">Brazil</option>
@@ -55,6 +50,15 @@ $Cfolder;
         <option value="7">Japan</option>
         <option value="8">South Korea</option>
     </select>
+    <br>
+    <?php
+    echo "<h2>Enter Your Name:";
+    ?>
+    <input type ="text" name = "keyword" placeholder = "Keyword" value = "<?=$_GET['keyword']?>"/>
+    <?php
+    echo "</h2>"
+    ?>
+    <br>
      <input type = "submit" value = "Submit"/>
      </form>
      <?php
@@ -63,55 +67,71 @@ $Cfolder;
              $CountryPics = $AustraliaPics;
              $Cfolder = 'austpics';
              $Song = 'Listening.mp3';
+             $backgroundImage = 'australianflag.gif';
          }
          if('2'==$countryNum){
              $CountryPics = $BrazilPics;
              $Cfolder = 'BrazilPics';
              $Song = 'MarisaMonte.mp3';
+             $backgroundImage = 'BrazilFlag.gif';
          }
          if('3'==$countryNum){
              $CountryPics = $ChinaPics;
              $Cfolder = 'ChinaPics';
              $Song = 'GEM.mp3';
+             $backgroundImage = 'chinese-flag.gif';
          }
          if('4'==$countryNum){
              $CountryPics = $DenmarkPics;
              $Cfolder = 'DenmarkPic';
              $Song = 'HarDuGlemt.mp3';
+             $backgroundImage = 'Denmark-flag.gif';
          }
          if('5'==$countryNum){
              $CountryPics = $EgyptPics;
              $Cfolder = 'EgyptPics';
              $Song ='HamzaNamira.mp3';
+             $backgroundImage = 'Egypt.gif';
          }
          if('6'==$countryNum){
              $CountryPics = $GreecePics;
              $Cfolder = 'GreecePics';
              $Song = 'ThanosPetrelis.mp3';
+             $backgroundImage = 'greece.gif';
          }
          if('7'==$countryNum){
              $CountryPics = $JapanPics;
              $Cfolder = 'JapanPics';
-             $Song = 'Yuna.mp3'; 
+             $Song = 'Yuna.mp3';
+             $backgroundImage = 'japanese-flag.gif';
          }
          if('8'==$countryNum){
              $CountryPics = $SKPics;
              $Cfolder = 'SouthKoreaPics';
              $Song = 'BigBang.mp3';
+             $backgroundImage = 'south-korea-flag.gif';
          }
          
      }
      if(isset($_GET['music'])){
          if($_GET['music']== "On"){
-         echo '<audio autoplay loop>
-            <source src= "'.$Music.'/'.$Song.' type=audio/mpeg">
+         echo "<audio autoplay loop>
+            <source src= Music/$Song type=audio/mpeg>
             Your browser does not support the audio element.
-            </audio>';
+            </audio>";
          }
-         if($_GET['music']== 'Off')
+         if($_GET['music']== "Off")
          {
-             echo "Music is off <br>";
+             echo "<h3>Music is off <h3>";
          }
+     }
+     if(isset($_GET['keyword']))
+     {
+        // echo "<h2>*</h2>";
+         //echo "<h1>";
+         echo "<h1>*Hello ". $_GET['keyword'].". I hope you enjoy this!!!*</h1>";
+         //echo "</h1>";
+        // echo "<h2>*</h2>";
      }
      ?>
          <div id  = "carousel-example-generic" class = "carousel slide" data-ride = "carousel">
@@ -158,4 +178,10 @@ $Cfolder;
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </body>
+    <style>
+        @import url("css/style.css");
+           body{
+               background-image:url(<?=$backgroundImage?>);
+           }
+    </style>
 </html>
